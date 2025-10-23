73030a12be64cf6496b565d26292a6ab2a488224
Antoni Balcerzak <antekbal06@gmail.com>
2025-10-23 11:56:52 +0200

    Feature 2 completed

--- src/account.py
+++ src/account.py
@@ -1,5 +1,6 @@
 class Account:
-    def __init__(self, first_name, last_name, balance):
+    def __init__(self, first_name, last_name, balance, pesel):
         self.first_name = first_name
         self.last_name = last_name
         self.balance = balance
+        self.pesel = pesel
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,8 +3,9 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe", 0)
+        account = Account("John", "Doe", 0, '05291201356')
         assert account.first_name == "John"
         assert account.last_name == "Doe"
         assert account.balance == 0
+        assert account.pesel == '05291201356'
 

666b59e6893b3f47c722891012167e2f88bc2eca
Antoni Balcerzak <antekbal06@gmail.com>
2025-10-23 12:01:17 +0200

    Feature 4 completed

--- src/account.py
+++ src/account.py
@@ -1,6 +1,10 @@
 class Account:
-    def __init__(self, first_name, last_name, balance, pesel):
+    def __init__(self, first_name, last_name, balance, pesel, promo_code=None):
         self.first_name = first_name
         self.last_name = last_name
-        self.balance = balance
+        if (promo_code == 'XYZ'):
+            self.balance = balance + 50
+        else:
+            self.balance = balance
         self.pesel = pesel
+        self.promo_code = promo_code
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,9 +3,13 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe", 0, '05291201356')
+        account = Account("John", "Doe", 0, '05291201356', 'XYZ')
         assert account.first_name == "John"
         assert account.last_name == "Doe"
-        assert account.balance == 0
         assert len(account.pesel) == 11
+        if (account.promo_code == 'XYZ'):
+            assert account.balance == 50
+        else:
+            assert account.balance == 0
+
 

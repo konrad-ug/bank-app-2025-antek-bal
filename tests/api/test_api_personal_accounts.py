f81d8b3c2c4c020a443a25fecba412a08bdf19a3
Antoni Balcerzak <antekbal06@gmail.com>
2025-12-08 17:09:15 +0100

    changes

--- tests/api/test_api_personal_accounts.py
+++ tests/api/test_api_personal_accounts.py
@@ -1,42 +1,30 @@
-from app.api import account_registry
+import requests
+import pytest
+
+BASE_URL = "http://localhost:3000/api/personal_accounts"
 
 class TestPersonalAccountsApi:
     
-    def test_create_account(self, client):
-        data = {"first_name": "Jan", "last_name": "Kowalski", "pesel": "12345678901"}
-        response = client.post("/api/personal_accounts", json=data)
+    @pytest.fixture
+    def created_account(self):
+        """
+        Ta fixtura:
+        1. Tworzy dane
+        2. Czyści serwer po teście (TEARDOWN)
+        """
+        pesel = "12345678901"
+        data = {"first_name": "Jan", "last_name": "Kowalski", "pesel": pesel}
         
+        yield data
+        
+        requests.delete(f"{BASE_URL}/{pesel}")
+
+    def test_create_account(self, created_account):
+        response = requests.post(BASE_URL, json=created_account)
         assert response.status_code == 201
-        assert len(account_registry.personal_accounts) == 1
 
-    def test_get_account_by_pesel(self, client):
-        data = {"first_name": "Jan", "last_name": "Kowalski", "pesel": "12345678901"}
-        client.post("/api/personal_accounts", json=data)
-
-        response = client.get("/api/personal_accounts/12345678901")
+    def test_get_account(self, created_account):
+        requests.post(BASE_URL, json=created_account)
+        
+        response = requests.get(f"{BASE_URL}/{created_account['pesel']}")
         assert response.status_code == 200
-        assert response.get_json()["first_name"] == "Jan"
-
-    def test_searching_by_invalid_pesel(self, client):
-        response = client.get("/api/personal_accounts/00000000000")
-        assert response.status_code == 404
-
-    def test_update_account(self, client):
-        data = {"first_name": "Jan", "last_name": "Kowalski", "pesel": "12345678901"}
-        client.post("/api/personal_accounts", json=data)
-
-        update_data = {"first_name": "Adam", "last_name": "Nowak"}
-        response = client.patch("/api/personal_accounts/12345678901", json=update_data)
-        assert response.status_code == 200
-
-        get_response = client.get("/api/personal_accounts/12345678901")
-        assert get_response.get_json()["first_name"] == "Adam"
-        assert get_response.get_json()["last_name"] == "Nowak" 
-
-    def test_delete_account(self, client):
-        data = {"first_name": "Jan", "last_name": "Kowalski", "pesel": "12345678901"}
-        client.post("/api/personal_accounts", json=data)
-
-        response = client.delete("/api/personal_accounts/12345678901")
-        assert response.status_code == 200
-        assert len(account_registry.personal_accounts) == 0
